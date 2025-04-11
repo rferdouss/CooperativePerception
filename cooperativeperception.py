a6c7358afe353ec7a996fd6d226b513f8fb75257
Raihana Ferdous <84193541+rferdouss@users.noreply.github.com>
2025-04-11 07:22:31 +0200

    Research on Cooperative Perception

---
+++ cooperativeperception.py
@@ -0,0 +1,14 @@
+
+def Define_CAV_List():
+    cavlist={}
+    return cavlist
+
+def LoadSensorData():
+    SensorData={}
+    return SensorData
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    inputpathofSensorData = ""
+    CAV_List =  Define_CAV_List()
+    Sensordata = LoadSensorData()
